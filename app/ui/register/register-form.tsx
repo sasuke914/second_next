7c00af2fe0290bda7df5f899262d77fb72fc187d
sasuke914 <takedaharuhito0914@gmail.com>
2024-09-03 13:29:17 -0700

    qwer

--- app/ui/register/register-form.tsx
+++ app/ui/register/register-form.tsx
@@ -13,7 +13,6 @@ import { ChangeEvent, FormEvent, useState } from 'react';
 import { useRouter } from 'next/navigation';
 
 export default function RegisterForm() {
-
     const [formData, setFormData] = useState({
         email: '',
         password: '',
@@ -30,7 +29,7 @@ export default function RegisterForm() {
         try {
             const userCredential = await createUserWithEmailAndPassword(auth, formData.email, formData.password);
             const user = userCredential.user;
-            console.log("Users:", user)
+            console.log('User:', user);
 
             await setDoc(doc(db, 'users', user.uid), {
                 uid: user.uid,
@@ -39,31 +38,26 @@ export default function RegisterForm() {
                 lastName: formData.lastName,
                 createdAt: new Date().toISOString(),
             });
-            console.log('User created and data saved!');
-            router.push('/login')
 
+            console.log('User created and data saved!');
+            router.push('/login');
         } catch (err) {
             setError((err as Error).message);
             console.error(err);
         }
-    }
+    };
 
-    const handleChange = (e: ChangeEvent<HTMLFormElement>) => {
-        setFormData({ ...formData, [e.target.name]: e.target.value })
-    }
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
 
     return (
-        <form className="space-y-3">
+        <form className="space-y-3" onSubmit={handleSignup}>
             <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
-                <h1 className={`${lusitana.className} mb-3 text-2xl`}>
-                    Please register to continue.
-                </h1>
+                <h1 className={`${lusitana.className} mb-3 text-2xl`}>Please register to continue.</h1>
                 <div className="w-full">
                     <div>
-                        <label
-                            className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-                            htmlFor="firstName"
-                        >
+                        <label className="mb-3 mt-5 block text-xs font-medium text-gray-900" htmlFor="firstName">
                             First Name
                         </label>
                         <div className="relative">
@@ -80,10 +74,7 @@ export default function RegisterForm() {
                         </div>
                     </div>
                     <div>
-                        <label
-                            className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-                            htmlFor="lastName"
-                        >
+                        <label className="mb-3 mt-5 block text-xs font-medium text-gray-900" htmlFor="lastName">
                             Last Name
                         </label>
                         <div className="relative">
@@ -100,10 +91,7 @@ export default function RegisterForm() {
                         </div>
                     </div>
                     <div>
-                        <label
-                            className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-                            htmlFor="email"
-                        >
+                        <label className="mb-3 mt-5 block text-xs font-medium text-gray-900" htmlFor="email">
                             Email
                         </label>
                         <div className="relative">
@@ -120,10 +108,7 @@ export default function RegisterForm() {
                         </div>
                     </div>
                     <div className="mt-4">
-                        <label
-                            className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-                            htmlFor="password"
-                        >
+                        <label className="mb-3 mt-5 block text-xs font-medium text-gray-900" htmlFor="password">
                             Password
                         </label>
                         <div className="relative">
@@ -141,17 +126,13 @@ export default function RegisterForm() {
                         </div>
                     </div>
                 </div>
-                <Button onClick={handleSignup} className="mt-4 w-full">
+                <Button type="submit" className="mt-4 w-full">
                     Register <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
                 </Button>
-                <div className='flex justify-end mt-[10px] text-gray-500 text-sm'>
-                    <Link href={'/login'}>
-                        Already registered
-                    </Link>
+                <div className="flex justify-end mt-[10px] text-gray-500 text-sm">
+                    <Link href={'/login'}>Already registered</Link>
                 </div>
-                <div className="flex h-8 items-end space-x-1">
-                    {error && <p>{error}</p>}
-                </div>
+                <div className="flex h-8 items-end space-x-1">{error && <p>{error}</p>}</div>
             </div>
         </form>
     );
